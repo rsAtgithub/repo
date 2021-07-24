bdc1b34d341918ff769b523cd961c456cc5c4c66
Rushikesh Shingnapurkar <rvshingnapurkar@gmail.com>
2021-07-24 09:26:58 +0530

    ARM Simu with Debug Print and Instrumentation - 2

--- armSimuDebugPrintf_Instrument/systemLogging.c
+++ armSimuDebugPrintf_Instrument/systemLogging.c
@@ -9,11 +9,11 @@ void __cyg_profile_func_enter(void *current_func, void *callsite) __attribute__(
 void __cyg_profile_func_exit(void *current_func, void *callsite) __attribute__((no_instrument_function));
 
 void __cyg_profile_func_enter(void *current_func, void *callsite) {
-		printf("Entry,%08x,%d\n", (int)current_func,*(volatile unsigned int *)0x20000800);
+		printf("Entry,%08x,%08x,%d\n", (int)current_func,(int)callsite,*(volatile unsigned int *)0x20000800);
 		(void) callsite;
 }
 
 void __cyg_profile_func_exit(void *current_func, void *callsite) {
-	printf("Exit,%08x,%d\n", (int)current_func,*(volatile unsigned int *)0x20000800);
+	printf("Exit,%08x,%08x,%d\n", (int)current_func,(int)callsite,*(volatile unsigned int *)0x20000800);
 	(void) callsite;
 }
